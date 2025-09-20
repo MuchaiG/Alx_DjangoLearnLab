a925576320d869f7e66097d12a42c07593349e62
MuchaiG <dennisgachuru2@gmail.com>
2025-09-20 19:16:59 +0300

    Fixing errors

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
@@ -23,3 +24,18 @@ def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return HttpResponse(f"Deleted {book.title} (dummy response)")
 
+def search_books(request):
+    results = []
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['search_query']
+            # Secure ORM query (prevents SQL injection)
+            results = Book.objects.filter(title__icontains=query)
+    else:
+        form = ExampleForm()
+
+    return render(request, "bookshelf/book_list.html", {
+        "form": form,
+        "results": results
+    })
