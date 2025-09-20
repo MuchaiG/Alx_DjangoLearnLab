38bd6e6e14da6399479a15f0cc593c6035ef99d7
MuchaiG <dennisgachuru2@gmail.com>
2025-09-20 14:55:45 +0300

    Fixing errors

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,17 +1,18 @@
 from django.db import models
-from django.conf import settings  # <-- important, use this instead of importing User directly
+from django.conf import settings   # <- use this instead of importing User directly
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_date = models.DateField(null=True, blank=True)
+    published_date = models.DateField(null=True, blank=True)  # <-- fix applied
     borrower = models.ForeignKey(
-        settings.AUTH_USER_MODEL,   # <-- this points to accounts.CustomUser
+        settings.AUTH_USER_MODEL,   # <-- points to accounts.CustomUser
         on_delete=models.SET_NULL,
-        null=True, 
+        null=True,
         blank=True,
         related_name="borrowed_books"
     )
 
     def __str__(self):
         return self.title
+
