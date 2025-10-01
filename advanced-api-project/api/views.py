b151affdcc2c127762b3cc6955b1e29840e7747c
MuchaiG <dennisgachuru2@gmail.com>
2025-10-01 14:50:00 +0300

    Fix: Syntax errors

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 # List all books – anyone can view
 class BookListView(generics.ListAPIView):
