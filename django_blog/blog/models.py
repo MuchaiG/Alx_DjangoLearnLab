51b9f79eaf1e0bc89e82ba11403269e9b80ad535
MuchaiG <dennisgachuru2@gmail.com>
2025-10-04 19:27:07 +0300

    Fix: syntax errors

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
