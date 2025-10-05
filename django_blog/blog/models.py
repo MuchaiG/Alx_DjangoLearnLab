3eeee3c121ebee011641faa940b565b74508e324
MuchaiG <dennisgachuru2@gmail.com>
2025-10-05 08:13:34 +0300

    Update project with recent changes (auth, profile, views, etc.)

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +11,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'Users',    
 ]
 
 MIDDLEWARE = [
@@ -121,8 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
